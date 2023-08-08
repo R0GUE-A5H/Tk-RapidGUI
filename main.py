5766eef29cbb252401b2b6176bb7702eecbd6ce2
Ashwin <ashwin456ojha@gmail.com>
2023-08-08 17:00:48 +0530

    Removed Tools Window and added cool features

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.ttk import Progressbar
 
 root = tk.Tk()
 root.title("Tkinter Rapid Development")
@@ -14,6 +15,8 @@ widgets = {}  # Use a single dictionary to store widgets of different types
 widget_count = {}  # Use a dictionary to store counts for different widget types
 
 _count = 0
+
+
 
 def on_button_press(event, widget):
     widget.is_dragging = True
@@ -32,7 +35,6 @@ def on_button_release(event, widget, widget_dict):
 
 def instantiate_widget(widgetType, count_key, dict_widget):
     global _count
-
     _count += 1
 
     widgetType.place(x=root.winfo_width() // 2, y=root.winfo_height() // 2, anchor="center")
@@ -62,13 +64,31 @@ def instantiate_label():
     new_label = tk.Label(root, text="Dynamic Label")
     instantiate_widget(new_label, 'label', widgets)
 
-button_create = tk.Button(new_window, text="New Button", command=instantiate_button)
-button_create.place(x=15, y=10)
+def instantiate_entry():
+    new_entry = tk.Entry(root)
+    instantiate_widget(new_entry, 'entry', widgets)
 
-label_create = tk.Button(new_window, text="New Label", command=instantiate_label)
-label_create.place(x=15, y=40)
+def instantiate_progressbar():
+    new_bar = Progressbar(root)
+    instantiate_widget(new_bar, 'progressbar', widgets)
 
+def resize_widget(event):
+    pass
+    
+menu = tk.Menu(root, tearoff = 0)
+menu.add_command(label ="New Button", command=instantiate_button)
+menu.add_command(label ="New Label", command=instantiate_label)
+menu.add_command(label ="New Entry", command=instantiate_entry)
+menu.add_separator()
+menu.add_command(label ="ProgressBar", command=instantiate_progressbar)
+menu.add_separator()
+menu.add_command(label ="CheckButton")
+menu.add_command(label="RadioButton")
+menu.add_command(label="Combobox")
+menu.add_separator()
+menu.add_command(label="Scale")
+menu.add_command(label="Scrollbar")
 
-
-
+root.bind("<Button-3>", lambda event: menu.tk_popup(event.x_root, event.y_root))
+root.bind("<Configure>", resize_widget)
 root.mainloop()
