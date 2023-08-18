849d7d7507b9260c179b4bd6aa0b0bb8353eb5d8
Ashwin <ashwin456ojha@gmail.com>
2023-08-18 10:15:43 +0530

    commit

--- GeneratedPyFile.py
+++ GeneratedPyFile.py
@@ -10,48 +10,12 @@ def funcButton1():
     pass
 
 Button1 = tk.Button(root, text="Button1", command="funcButton1")
-Button1.place(x=42, y=58)
+Button1.place(x=388, y=75)
 
-def funcLabel2():
+def funcButton2():
     pass
 
-Label2 = tk.Label(root, text="Label2")
-Label2.place(x=52, y=92)
-
-def funcEntry3():
-    pass
-
-Entry3 = tk.Entry(root)
-Entry3.place(x=154, y=91)
-
-def funcProgressbar4():
-    pass
-
-Progressbar4 = Progressbar(root)
-Progressbar4.place(x=111, y=138)
-
-def funcButton5():
-    pass
-
-Button5 = tk.Button(root, text="Button5", command="funcButton5")
-Button5.place(x=321, y=83)
-
-def funcEntry6():
-    pass
-
-Entry6 = tk.Entry(root)
-Entry6.place(x=307, y=54)
-
-def funcLabel7():
-    pass
-
-Label7 = tk.Label(root, text="Label7")
-Label7.place(x=23, y=139)
-
-def funcEntry8():
-    pass
-
-Entry8 = tk.Entry(root)
-Entry8.place(x=320, y=160)
+Button2 = tk.Button(root, text="Button2", command="funcButton2")
+Button2.place(x=393, y=132)
 
 root.mainloop()
